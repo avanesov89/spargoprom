217b636209fb3c4691856e4bbcb48285ec7a1753
«yura» <avanesov89@mail.ru>
2021-11-18 12:19:26 +0300

    edit files

--- js/main.js
+++ js/main.js
@@ -4,11 +4,24 @@ console.log('global');
 "use strict";
 
 console.log('maxgraph');
+"use strict";
+
 /**
   * название функции
   *
   * @param {number} first - первое число
   * @returns {number}
   */
-"use strict";
+var details = Array.from(document.querySelectorAll("details")); // Add the onclick listeners.
+
+details.forEach(function (targetDetail) {
+  targetDetail.addEventListener("click", function () {
+    // Close all the details that are not targetDetail.
+    details.forEach(function (detail) {
+      if (detail !== targetDetail) {
+        detail.removeAttribute("open");
+      }
+    });
+  });
+});
 //# sourceMappingURL=main.js.map
