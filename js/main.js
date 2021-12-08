ea1c56f2c5232887cba7a75f6b19667b990ae37e
yu.avanesov <avanesov89@mail.ru>
2021-12-08 15:22:00 +0300

    edit files

--- js/main.js
+++ js/main.js
@@ -78,4 +78,19 @@ $('.clients__list').slick({
   slidesToShow: 4,
   slidesToScroll: 4
 });
+var fileInput = document.querySelector('.custom-file input');
+
+function declOfNum(number, titles) {
+  cases = [2, 0, 1, 1, 1, 2];
+  return titles[number % 100 > 4 && number % 100 < 20 ? 2 : cases[number % 10 < 5 ? number % 10 : 5]];
+}
+
+fileInput.addEventListener('change', function (e) {
+  console.log(e.currentTarget.files);
+  var filesLength = e.currentTarget.files.length;
+
+  if (filesLength) {
+    e.currentTarget.closest('.custom-file').querySelector('span').textContent = "".concat(filesLength, " ").concat(declOfNum(filesLength, ['файл', 'файла', 'файлов']));
+  }
+});
 //# sourceMappingURL=main.js.map
