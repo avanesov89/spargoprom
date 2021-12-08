110037889af997ddcdec3a30c1ccae98548d0ccd
yu.avanesov <avanesov89@mail.ru>
2021-12-08 12:47:45 +0300

    edit files

--- js/main.js
+++ js/main.js
@@ -12,7 +12,26 @@ console.log('maxgraph');
   * @param {number} first - первое число
   * @returns {number}
   */
-console.log('test');
+$(function () {
+  var btn = $(".header__search"),
+      block = $(".search__fixed");
+  $(document).click(function (event) {
+    var target = $(event.target),
+        cur;
+
+    if (target.is(".header__search")) {
+      event.preventDefault();
+      var index = btn.index(target);
+      cur = block.eq(index).slideToggle();
+      target.toggleClass("open");
+    }
+
+    if (target.closest(".search__fixed").length) return;
+    block.not(cur).slideUp("slow");
+    btn.not(target).removeClass("open");
+  });
+}); // Accordeon
+
 var details = Array.from(document.querySelectorAll("details")); // Add the onclick listeners.
 
 details.forEach(function (targetDetail) {
@@ -52,26 +71,6 @@ $('.menu__item-drop').on('click', function () {
 
   ;
 });
-$('.show-right').on('click', function (event) {
-  $('.side-left').addClass('shifted');
-  $('.side-right').removeClass('shifted');
-});
-$('.show-left').on('click', function (event) {
-  $('.side-right').addClass('shifted');
-  $('.side-left').removeClass('shifted');
-});
-$(".header__search").click(function () {
-  $(".search__fixed").css({
-    "display": "block"
-  });
-});
-$(document).mouseup(function (e) {
-  var div = $(".search__fixed");
-
-  if (!div.is(e.target) && div.has(e.target).length === 0) {
-    div.hide();
-  }
-});
 $('.clients__list').slick({
   dots: true,
   infinite: false,
@@ -79,23 +78,4 @@ $('.clients__list').slick({
   slidesToShow: 4,
   slidesToScroll: 4
 });
-
-window.onload = function () {
-  document.getElementById('toggler').onclick = function () {
-    openbox('box', this);
-    return false;
-  };
-};
-
-function openbox(id, toggler) {
-  var div = document.getElementById(id);
-
-  if (div.style.display == 'block') {
-    div.style.display = 'none';
-    toggler.innerHTML = 'Открыть';
-  } else {
-    div.style.display = 'block';
-    toggler.innerHTML = 'Закрыть';
-  }
-}
 //# sourceMappingURL=main.js.map
